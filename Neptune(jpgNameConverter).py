6f019bdbf0cc8f05e9f68c3945cbb5006efde928
Juliansh787 <59464137+Juliansh787@users.noreply.github.com>
2021-05-31 15:42:08 +0900

    Update Neptune(jpgNameConverter).py

--- Neptune(jpgNameConverter).py
+++ Neptune(jpgNameConverter).py
@@ -1,17 +1,26 @@
+import sys
 from sys import argv
+from os.path import join, dirname, abspath
 
 # Main Dialog
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtWidgets import QFileDialog
-from os import chdir, getcwd, rename, mkdir
-from PIL import Image
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QFileDialog
+from os import chdir, getcwd, mkdir
+from cv2 import IMREAD_GRAYSCALE, IMREAD_COLOR, imdecode, imwrite, getRotationMatrix2D, warpAffine
+from numpy import fromfile, uint8
 
-mainDlg_class = uic.loadUiType("ui/txtGenerator.ui")[0]
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', dirname(abspath(__file__)))
+    return join(base_path, relative_path)
+form = resource_path("..\\envs\\jpgConverter.ui")
+mainDlg_class = uic.loadUiType(form)[0]
 
-class txtGenerator(QMainWindow, mainDlg_class):
+# mainDlg_class = uic.loadUiType("ui/jpgConverter.ui")[0]
+
+class jpgConverter(QMainWindow, mainDlg_class):
     def __init__(self):
-        super(txtGenerator, self).__init__()
+        super(jpgConverter, self).__init__()
         self.setupUi(self)
 
         # Make directory to save result files
@@ -21,6 +30,7 @@ class txtGenerator(QMainWindow, mainDlg_class):
             pass
 
         self.default_path = getcwd()
+        self.rotateAngle = 0
 
         self.loadState = 'loading..\n'
         self.loadList = []
@@ -29,7 +39,7 @@ class txtGenerator(QMainWindow, mainDlg_class):
         self.startBtn.clicked.connect(self.startBtnFunction)
         self.resetBtn.clicked.connect(self.resetBtnFunction)
 
-        # self.textEdit_jpgList # txt만들면서 만들어지는 목록들 보여주기 => 그냥 보기좋으라고 있는거
+        self.err_code = None
 
     def loadBtnFunction(self):
         self.progressBar.setValue(0)
@@ -63,35 +73,81 @@ class txtGenerator(QMainWindow, mainDlg_class):
             pass
 
     def startBtnFunction(self):
-        self.targetName = str(self.textEdit_targetName.toPlainText())
-        currentState = 'converting..\n'
-        chdir(self.default_path)
+        try:
+            self.progressBar.setValue(0)
+            self.progressBar.setMaximum(len(self.loadList))
+            filecnt = 0
 
-        for i, name in enumerate(self.loadList):
-            temp = name.split('/')
-            oldName = temp[-1]
-            newName = self.targetName + '_' + str(i) + '.jpg'
-            currentState = currentState + str(i) + ' : ' + oldName + ' => ' + newName + '\n'
+            if str(self.textEdit_targetName.toPlainText()) != '':
+                self.targetName = str(self.textEdit_targetName.toPlainText())
+            else:
+                self.err_code = 0
+                raise ValueError
 
-            # path = name.replace(oldName, '')
-            Image.open(name).convert('RGB').save('./result/' + newName)
+            if str(self.textEdit_rotate.toPlainText()) != '':
+                self.rotateAngle = float(self.textEdit_rotate.toPlainText())
+
+            currentState = 'converting..\n'
+            chdir(self.default_path)
+
+            for i, name in enumerate(self.loadList):
+                temp = name.split('/')
+                oldName = temp[-1]
+                newName = self.targetName + '_' + str(i) + '.jpg'
+                currentState = currentState + str(i) + ' : ' + oldName + ' => ' + newName + '\n'
+
+                if self.checkBox_gray.isChecked():
+                    ff = fromfile(name, uint8)  # 한글경로 변환
+                    img = imdecode(ff, IMREAD_GRAYSCALE)  # gray Scale로 변환, img = array
+                    height, width = img.shape
+                else:
+                    ff = fromfile(name, uint8)  # 한글경로 변환
+                    img = imdecode(ff, IMREAD_COLOR)  # img = array
+                    height, width, channel = img.shape
+
+                if self.rotateAngle != 0:
+                    matrix = getRotationMatrix2D((width / 2, height / 2), self.rotateAngle, 1)
+                    img = warpAffine(img, matrix, (width, height))
+
+                imwrite('./result/' + newName, img)
+
+                self.textEdit_jpgList.setText(currentState)
+
+                filecnt += 1
+                self.progressBar.setValue(filecnt)
+
+            currentState = currentState + 'Finish!'
             self.textEdit_jpgList.setText(currentState)
+            self.loadState = 'loading..\n'
+            self.loadList = []
 
-        currentState = currentState + 'Finish!'
-        self.textEdit_jpgList.setText(currentState)
-        self.loadState = 'loading..\n'
-        self.loadList = []
+            self.startBtn.setEnabled(False)
 
-        self.startBtn.setEnabled(False)
+            reply = QMessageBox.question(self, 'Message', 'Do you want to convert more files?',
+                                         QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                pass
+            else:
+                sys.exit()
+        except Exception as e:
+            if self.err_code == 0:
+                QMessageBox.critical(self, "ERROR!!", "Set Target Name!")
+            else:
+                QMessageBox.critical(self, str(e))
+            self.err_code = None
 
     def resetBtnFunction(self):
+        self.textEdit_targetName.setText('')
+        self.textEdit_rotate.setText('')
         self.startBtn.setEnabled(False)
+        self.checkBox_gray.setChecked(False)
+        self.rotateAngle = None
         self.loadList = []
         self.jpgList = []
         self.progressBar.setValue(0)
 
 if __name__ == "__main__":
     app = QApplication(argv)
-    win = txtGenerator()
+    win = jpgConverter()
     win.show()
     app.exec_()
