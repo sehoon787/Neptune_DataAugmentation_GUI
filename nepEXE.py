d94c948975b68127ee0af04c7a5d1735d6cd8504
Juliansh787 <59464137+Juliansh787@users.noreply.github.com>
2021-05-31 16:17:36 +0900

    Delete nepEXE.py

--- nepEXE.py
+++
@@ -1,8 +0,0 @@
-from os import system
-
-# pyinstaller --noconsole --onefile --icon=../icons/neptune.ico "nepEXE.py"
-
-try:
-    system('start dist\\Neptune(jpgNameConverter)\\Neptune(jpgNameConverter).exe')
-except:
-    pass
